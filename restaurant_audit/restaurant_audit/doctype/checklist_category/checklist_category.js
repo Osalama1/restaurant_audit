d60cf2d3cfadf6abd4d6f7557472c86131faf1f2
Osalama1 <omarsalama@gmail.com>
2025-07-28 12:45:01 +0200

    feat: Change Checklist Category from child table to Main doctype

---
+++ restaurant_audit/restaurant_audit/doctype/checklist_category/checklist_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Ontime Solutions and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Checklist Category", {
+// 	refresh(frm) {
+
+// 	},
+// });
